7b611ba8044e9d43e7dfb134d123450902cb4460
Alex Singleton <alexanderjsingleton@alexs-imac.cisco>
2017-04-23 19:19:49 -0500

    Alex updated Maintenance_Log table; revised Date to Maintenance_Date

---
+++ Queries-Tom.sql
@@ -0,0 +1,19 @@
+SELECT EmployeeID, EmployeeLName, EmployeeFName
+FROM EMPLOYEE
+GROUP BY EmployeeLName;
+ 
+SELECT VIN, CustomerID, Total
+FROM VEHICLE, RESERVATION
+ORDER BY Total;
+
+SELECT *
+FROM MAINTENANCE_LOG
+ORDER BY Date;
+
+SELECT RentalOfficeID, Pick_Up_Date, Return_Date, Daily_Rate, Total
+FROM RESERVATION
+GROUP BY RentalOfficeID;
+
+Select Type, Cost
+FROM ACCESSORY
+ORDER BY Cost DESC;
